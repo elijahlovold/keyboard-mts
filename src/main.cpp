b231d6a40cad018742b4f1cdaa55422e3b4b313d
elovold <elovold@cadenceneuro.com>
2024-11-24 22:01:15 -0800

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "pico/stdlib.h"
+
+// #include "tusb.h"
+// #include "tusb_config.h"
+
+// #include "usb_descriptors.h"
+
+/*------------- MAIN -------------*/
+int main(void) {
+    gpio_set_dir();
+	while (1) {
+
+
+
+	}
+}
+
